ef92cdc01ffaaddc37f51cf3f73d652873d9b0ca
MuGaber <mohammedgaber273@gmail.com>
2020-03-12 04:55:52 +0200

    testing with chai

--- server.js
+++ server.js
@@ -1,46 +1,124 @@
-const path = require('path')
-require('dotenv').config()
+'use strict'
+var express = require('express')
+var app = express()
 
-const express = require('express')
-const app = express()
+var cors = require('cors')
+var runner = require('./test-runner')
 
-// middlewares
-app.use(function(req, res, next) {
-  res.set({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers':
-      'Origin, X-Requested-With, content-type, Accept'
-  })
-  app.disable('x-powered-by')
-  next()
-})
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
-// using bcrypt for incryptin passords
-const bcrypt = require('bcrypt')
-
-const plaintextPassword = 'hithere'
-const saltRounds = 13
-
-// async
-bcrypt.hash(plaintextPassword, saltRounds, function(err, encHash) {
-  console.log(encHash) // $2b$13$T82Lqb5PUWz89HpFLelOFuGRa.WFXyqqgQQmvJUeBtAzv5anun/n2
-  bcrypt.compare(plaintextPassword, encHash, function(err, same) {
-    console.log(same)
-  })
-})
-
-// sync
-const hash = bcrypt.hashSync(plaintextPassword, saltRounds)
-const same = bcrypt.compareSync(plaintextPassword, hash)
-console.log(hash, same) // $2b$13$BJe1CnyrqgfkNYGRi1aC0Oj06wGsuEc7aA3qkhxP1V9KYvG.NQHmO
-
-// static files
-app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html')
+  res.sendFile(__dirname + '/views/index.html')
 })
 
-// running the server
-// app.listen(process.env.PORT, function() {
-//   console.log(`Server running at port ${process.env.PORT}`)
-// })
+app.use(express.static(__dirname + '/public'))
+
+app.get('/hello', function(req, res) {
+  var name = req.query.name || 'Guest'
+  res.type('txt').send('hello ' + name)
+})
+
+var travellers = function(req, res) {
+  var data = {}
+  if (req.body && req.body.surname) {
+    switch (req.body.surname.toLowerCase()) {
+      case 'polo':
+        data = {
+          name: 'Marco',
+          surname: 'Polo',
+          dates: '1254 - 1324'
+        }
+        break
+      case 'colombo':
+        data = {
+          name: 'Cristoforo',
+          surname: 'Colombo',
+          dates: '1451 - 1506'
+        }
+        break
+      case 'vespucci':
+        data = {
+          name: 'Amerigo',
+          surname: 'Vespucci',
+          dates: '1454 - 1512'
+        }
+        break
+      case 'da verrazzano':
+      case 'verrazzano':
+        data = {
+          name: 'Giovanni',
+          surname: 'da Verrazzano',
+          dates: '1485 - 1528'
+        }
+        break
+      default:
+        data = {
+          name: 'unknown'
+        }
+    }
+  }
+  res.json(data)
+}
+
+app.route('/travellers').put(travellers)
+
+var error
+app.get(
+  '/_api/get-tests',
+  cors(),
+  function(req, res, next) {
+    if (error || process.env.SKIP_TESTS)
+      return res.json({ status: 'unavailable' })
+    next()
+  },
+  function(req, res, next) {
+    if (!runner.report) return next()
+    res.json(testFilter(runner.report, req.query.type, req.query.n))
+  },
+  function(req, res) {
+    runner.on('done', function(report) {
+      process.nextTick(() =>
+        res.json(testFilter(runner.report, req.query.type, req.query.n))
+      )
+    })
+  }
+)
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log('Listening on port ' + process.env.PORT)
+  if (!process.env.SKIP_TESTS) {
+    console.log('Running Tests...')
+    setTimeout(function() {
+      try {
+        runner.run()
+      } catch (e) {
+        error = e
+        console.log('Tests are not valid:')
+        console.log(error)
+      }
+    }, 1500)
+  }
+})
+
+module.exports = app // for testing
+
+function testFilter(tests, type, n) {
+  var out
+  switch (type) {
+    case 'unit':
+      out = tests.filter(t => t.context.match('Unit Tests'))
+      break
+    case 'functional':
+      out = tests.filter(
+        t => t.context.match('Functional Tests') && !t.title.match('#example')
+      )
+      break
+    default:
+      out = tests
+  }
+  if (n !== undefined) {
+    return out[n] || out
+  }
+  return out
+}

97256d36ccfdfdd39ab78d60ea4aaf571fdcdba3
MuGaber <mohammedgaber273@gmail.com>
2020-03-13 05:02:37 +0200

    github OAuth

--- server.js
+++ server.js
@@ -1,52 +1,135 @@
-const bodyParser = require('body-parser')
+'use strict'
+
 const express = require('express')
-require('dotenv').config()
-
+const bodyParser = require('body-parser')
 const session = require('express-session')
+const mongo = require('mongodb').MongoClient
 const passport = require('passport')
-
-const routes = require('./routes')
-const auth = require('./auth')
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-const db = mongoose.connection
+var GitHubStrategy = require('passport-github').Strategy
+require('dotenv').config()
 
 const app = express()
 
+app.use('/public', express.static(process.cwd() + '/public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.set('view engine', 'pug')
-app.use('/public', express.static(process.cwd() + '/public'))
 
-app.use(
-  session({
-    secret: process.env.SECRET_KEY,
-    saveUninitialized: true,
-    resave: true
-  })
+mongo.connect(
+  process.env.DB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err, db) => {
+    if (err) {
+      console.log('Database error: ' + err)
+    } else {
+      console.log('Successful database connection')
+
+      app.use(
+        session({
+          secret: process.env.SECRET_KEY,
+          resave: true,
+          saveUninitialized: true
+        })
+      )
+
+      app.use(passport.initialize())
+      app.use(passport.session())
+
+      function ensureAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) {
+          return next()
+        }
+        res.redirect('/')
+      }
+
+      passport.serializeUser((user, done) => {
+        done(null, user.id)
+      })
+
+      passport.deserializeUser((id, done) => {
+        db.collection('socialusers').findOne({ id: id }, (err, doc) => {
+          done(null, doc)
+        })
+      })
+
+      /*
+       *  ADD YOUR CODE BELOW
+       */
+
+      passport.use(
+        new GitHubStrategy(
+          {
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL: 'https://glitch.com/~glowing-warm-bay/auth/github/callback'
+          },
+          function(accessToken, refreshToken, profile, cb) {
+            console.log(profile)
+
+            db.collection('socialusers').findAndModify(
+              { id: profile.id },
+              {},
+              {
+                $setOnInsert: {
+                  id: profile.id,
+                  name: profile.displayName || 'John Doe',
+                  photo: profile.photos[0].value || '',
+                  email: profile.emails[0].value || 'No public email',
+                  created_on: new Date(),
+                  provider: profile.provider || ''
+                },
+                $set: {
+                  last_login: new Date()
+                },
+                $inc: {
+                  login_count: 1
+                }
+              },
+              { upsert: true, new: true },
+              (err, doc) => {
+                return cb(null, doc.value)
+              }
+            )
+          }
+        )
+      )
+
+      app.get('/auth/github', passport.authenticate('github'))
+
+      app
+        .route('/auth/github/callback')
+        .get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+          res.redirect('/profile')
+        })
+
+      /*
+       *  ADD YOUR CODE ABOVE
+       */
+
+      app.route('/').get((req, res) => {
+        res.render(process.cwd() + '/views/pug/index')
+      })
+
+      app.route('/profile').get(ensureAuthenticated, (req, res) => {
+        res.render(process.cwd() + '/views/pug/profile', { user: req.user })
+      })
+
+      app.route('/logout').get((req, res) => {
+        req.logout()
+        res.redirect('/')
+      })
+
+      app.use((req, res, next) => {
+        res
+          .status(404)
+          .type('text')
+          .send('Not Found')
+      })
+
+      app.listen(process.env.PORT || 3000, () => {
+        console.log('Listening on port ' + process.env.PORT)
+      })
+    }
+  }
 )
-
-app.use(passport.initialize())
-app.use(passport.session())
-
-db.on('error', function(err) {
-  console.log('DB connection error', err)
-  mongoose.disconnect()
-})
-
-db.once('open', function() {
-  console.log('DB connected')
-
-  auth(app, db)
-
-  routes(app, db)
-
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('Listening on port ' + process.env.PORT)
-  })
-})
